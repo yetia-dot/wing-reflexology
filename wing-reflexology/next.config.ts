79f87926ddd536fe3f5e5f728170d9c4444ac779
infangle <lakewyetnayet93@gmail.com>
2025-11-12 01:01:02 +0300

    deploy update

--- wing-reflexology/next.config.ts
+++ wing-reflexology/next.config.ts
@@ -4,10 +4,4 @@ const nextConfig: NextConfig = {
   /* config options here */
 };
 
-// next.config.js
-module.exports = {
-  // Other Next.js configuration options...
-  output: "dist", // Specify the build output directory
-};
-
 export default nextConfig;
