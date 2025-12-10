3f1ccb859e8bd37dc0c3acef06ce3257e6a75a77
yetia-dot <yetnayetaramde@gmail.com>
2025-12-10 12:36:05 +0300

    update sitemap build

--- wing-reflexology/next-sitemap.config.js
+++ wing-reflexology/next-sitemap.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: 'https://wingreflexology.com', // your domain
-  generateRobotsTxt: true, // automatically creates robots.txt
-  sitemapSize: 7000, // optional, splits sitemap if large
-  changefreq: 'weekly', // optional, Google hint for update frequency
-  priority: 0.7,       // optional, page priority
+  siteUrl: 'https://wing-reflexology.vercel.app', // or your domain later
+  generateRobotsTxt: true,
+  outDir: './public', // <- ensures sitemap.xml & robots.txt are copied to public
 };
