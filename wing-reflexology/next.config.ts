a2fa14ab4531a55d5a99c526c8d7332cf7ce370e
yetia-dot <yetnayetaramde@gmail.com>
2025-12-10 11:54:24 +0300

    add sitemap

--- wing-reflexology/next.config.ts
+++
@@ -1,8 +0,0 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  output: "export", // enable static HTML export
-  // any other config you may have
-};
-
-export default nextConfig;
