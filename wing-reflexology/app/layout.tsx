e0573a60be803b26eebbc2c147e334286a92a252
yetia-dot <yetnayetaramde@gmail.com>
2025-12-15 11:10:30 +0300

    update for nextjs warning

--- wing-reflexology/app/layout.tsx
+++ wing-reflexology/app/layout.tsx
@@ -37,7 +37,7 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning={true}>
       <body className={`${inter.className} antialiased`} suppressHydrationWarning={true}>
         {children}
         <Analytics />
