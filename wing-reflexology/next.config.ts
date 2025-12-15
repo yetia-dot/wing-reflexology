f58d34c6bfce1c44e7e25b0cb4699c1cd680b0b2
yetia-dot <yetnayetaramde@gmail.com>
2025-12-15 16:01:18 +0300

    update image optimize for localhost

--- wing-reflexology/next.config.ts
+++ wing-reflexology/next.config.ts
@@ -4,6 +4,9 @@ const nextConfig: NextConfig = {
   output: "export",      // enable static HTML export
   trailingSlash: true,   // generate /about/index.html instead of about.html
   // any other config you may have
+  images:{
+    unoptimized: true,
+  }
 };
 
 export default nextConfig;
